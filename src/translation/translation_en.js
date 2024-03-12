7a7997df40067383646c95fe572e4483bcee8c1d
KEVIN ALEXSANDER DE JESUS <1050482123033@fatec.coorp>
2024-03-12 20:01:17 -0300

    translated the equipment table

--- src/translation/translation_en.js
+++ src/translation/translation_en.js
@@ -1,12 +1,17 @@
 const translation = {
   translation: {
+    "Description": "Description",
     "Equipment": "Equipment",
     "Equipments": "Equipments",
     "English": "English",
+    "ID": "ID",
     "Login": "Login",
     "Logout": "Logout",
+    "Name": "Name",
     "P.I 6th Period - 2024":"P.I 6th Period - 2024",
     "Portuguese": "Portuguese",
+    "Priority": "Priority",
+    "Type": "Type",
   }
 }
 
--- src/translation/translation_pt.js
+++ src/translation/translation_pt.js
@@ -1,12 +1,17 @@
 const translation = {
   translation: {
+    "Description": "Descrição",
     "Equipment": "Equipamento",
     "Equipments": "Equipamentos",
     "English": "Inglês",
+    "ID": "ID",
     "Login": "Entrar",
     "Logout": "Sair",
+    "Name": "Nome",
     "P.I 6th Period - 2024":"P.I 6° Semestre - 2024",
     "Portuguese": "Português",
+    "Priority": "Prioridade",
+    "Type": "Tipo",
   }
 }
 
